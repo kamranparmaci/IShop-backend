39ebc44ddd3a39bc3ffbb5ba6d5c37509289b884
kamran <kamranparmasii@gmail.com>
2023-01-29 20:35:52 +0330

    created validationAdmin and errorsAdmin file

---
+++ middlewares/error-handling/errorsAdmin.js
@@ -0,0 +1,26 @@
+const handleCreateSuperadminErrors = (err, req, res, next) => {
+  let errors = [];
+
+  // Check for validation errors
+  if (err.name === 'ValidationError') {
+    Object.values(err.errors).forEach((val) => errors.push(val.message));
+    return res.status(400).json({ success: false, errors });
+  }
+
+  // Check for MongoDB errors
+  if (err.code === 11000) {
+    return res.status(400).json({
+      success: false,
+      errors: [
+        {
+          message: 'A superadmin with this username already exists.',
+        },
+      ],
+    });
+  }
+
+  // Internal server error
+  return res
+    .status(500)
+    .json({ success: false, errors: [{ message: 'Internal server error' }] });
+};
---
+++ middlewares/validations/validateAdmin.js
@@ -0,0 +1,29 @@
+import { check, validationResult } from 'validator';
+
+const validateCreateSuperadmin = (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  // Check for valid username
+  check(username, 'Username is required').notEmpty();
+  check(username, 'Username must be at least 4 characters long').isLength({
+    min: 4,
+  });
+
+  // Check for valid email
+  check(email, 'Email is required').notEmpty();
+  check(email, 'Please enter a valid email').isEmail();
+
+  // Check for valid password
+  check(password, 'Password is required').notEmpty();
+  check(password, 'Password must be at least 8 characters long').isLength({
+    min: 8,
+  });
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+export { validateCreateSuperadmin };
