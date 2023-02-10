2d67de9b34887c7beba13fae8cd1412796045fcb
kamran <kamranparmasii@gmail.com>
2023-02-10 18:46:59 +0330

    fixed bugs in index.js and change the mongo url

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import helmet from "helmet";
-import morgan from "morgan";
-import { validateCreateSuperadmin } from "./middlewares/validations/validateAdmin.js";
-import handleCreateSuperadminErrors from "./middlewares/error-handling/errorsAdmin.js";
-import uploadImage from "./utils/upload.js";
-import { createSuperadmin } from "./controllers/admins.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { validateCreateSuperadmin } from './middlewares/validations/validateAdmin.js';
+import handleCreateSuperadminErrors from './middlewares/error-handling/errorsAdmin.js';
+import uploadImage from './utils/upload.js';
+import { createSuperadmin } from './controllers/admins.js';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose
@@ -39,16 +39,16 @@ app.use(helmet());
 // app.use(compression());
 
 // Use morgan for logging
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
 // Use body-parser to parse requests
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
 app.post(
-  "/superadmin/register",
+  '/superadmin/register',
   validateCreateSuperadmin,
   handleCreateSuperadminErrors,
-  uploadImage("single", "avatar"),
+  uploadImage('single', 'avatar'),
   createSuperadmin
 );
